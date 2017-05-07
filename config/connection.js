c424b1bed0d9f11e63a2874a73bf58b229891b55
AndyEm <andyem64@gmail.com>
2017-05-07 00:40:59 -0400

    heroku deployment did't work so reverting back to working code

--- config/connection.js
+++ config/connection.js
@@ -1,34 +1,21 @@
 // Require packages
 const mysql = require('mysql');
-var connection
 // Create connection
-// const connection = mysql.createConnection({
-//     host: 'localhost',
-//     user: 'root',
-//     password: 'root',
-//     database: 'burgers_db'
-// });
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'root',
+    database: 'burgers_db'
+});
 
 // Establish connection 
-// connection.connect(function(err) {
-//     if (err) {
-//         console.error("error connecting: " + err.stack);
-//         return;
-//     }
-//     console.log("connected as id " + connection.threadId);
-// });
+connection.connect(function(err) {
+    if (err) {
+        console.error("error connecting: " + err.stack);
+        return;
+    }
+    console.log("connected as id " + connection.threadId);
+});
 
-if(process.env.JAWSDB_URL) {
-	connection = mysql.createConnection(process.env.JAWSDB_URL);
-}
-else {
-	connection = mysql.createConnection({
-        port: 3306,
-        host: "localhost",
-        user: "root",
-        password: "",
-        database: "burger_db"
-	})
-}
 // Export connection for our ORM to use.
 module.exports = connection;
